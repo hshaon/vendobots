0d9d8a6c452024711287b286d462852d0993df4e
Azure2212 <Dasanbob22122002@gmail.com>
2025-11-02 01:12:30 -0400

    update more APIs for deliveryRecord

--- backend_server/app/routers/deliveryRecord.py
+++ backend_server/app/routers/deliveryRecord.py
@@ -7,12 +7,15 @@ from app import crud, schemas, database, models
 router = APIRouter(prefix="/deliveryRecord", tags=["deliveryRecord"])
 
 @router.get("/", response_model=list[schemas.DeliveryRecord])
-def get_logs(db: Session = Depends(database.get_db)):
-    print("vo")
+def get_delivery_record(db: Session = Depends(database.get_db)):
     return db.query(models.deliveryRecords).all()
 
 
 @router.post("/", response_model=schemas.DeliveryRecord)
 def create_delivery_record(record: schemas.DeliveryRecordCreate, db: Session = Depends(database.get_db)):
-    print("go")
     return crud.create_delivery_record(db, record)
+
+@router.get("/{robot_id}", response_model=list[schemas.DeliveryRecord])
+def get_delivery_record_by_robotID(robot_id: int, db: Session = Depends(database.get_db)):
+    records = db.query(models.deliveryRecords).filter(models.deliveryRecords.robot_id == robot_id).all()
+    return records
